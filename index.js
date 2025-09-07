22ba25429645c19f54018b462c5f0caa5d605041
daretechie <deeprince2020@gmail.com>
2025-09-07 04:41:59 +0100

    updated

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, CI/CD, Docker, AWS World!");
-  res.send("Hello, World!");
+  res.send("Hello, CI/CD, Docker, AWS World!\nHello, World!");
 });
 
 app.listen(port, () => {
