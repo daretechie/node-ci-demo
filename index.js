af446e36e183e249fc9e00b2315e8046a83d0f7b
daretechie <deeprince2020@gmail.com>
2025-09-08 19:43:42 +0100

    Refactor application structure by moving server logic to server.js and updating start script in package.json

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, CI/CD, Docker, AWS World!\nHello, World!");
-});
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
 });
 
 module.exports = app;
