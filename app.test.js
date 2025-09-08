800466772f7273df947dabd904e01775226fed9e
daretechie <deeprince2020@gmail.com>
2025-09-08 19:35:39 +0100

    Refactor tests to use server instance and update expected response

--- app.test.js
+++ app.test.js
@@ -1,8 +1,19 @@
 const request = require("supertest");
-const app = require("./index"); // export app in index.js
+const app = require("./index");
+let server;
 
-test("GET / should return Hello CI/CD World!", async () => {
-  const res = await request(app).get("/");
+beforeAll((done) => {
+  server = app.listen(0, () => {
+    done();
+  });
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+test("GET / should return Hello, CI/CD, Docker, AWS World!\nHello, World!", async () => {
+  const res = await request(server).get("/");
   expect(res.statusCode).toBe(200);
-  expect(res.text).toBe("Hello CI/CD World!");
+  expect(res.text).toBe("Hello, CI/CD, Docker, AWS World!\nHello, World!");
 });
